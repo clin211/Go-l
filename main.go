4e43fbd03184020275f061034b0b47e079fc22f0
Forest <767425412lin@gmail.com>
2022-07-09 22:38:42 +0800

    refactor: add go mod

--- main.go
+++ main.go
@@ -1,48 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"encoding/xml"
-	"fmt"
-	"log"
-	"net/http"
-)
-
-type Customer struct {
-	Name    string `json:"full_name" xml:"name"`
-	City    string `json:"city" xml:"city"`
-	ZipCode string `json:"zip_code" xml:"zipcode"`
-}
+import "banking/app"
 
 func main() {
-	// define routes
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
-
-	// starting server
-	log.Fatal(http.ListenAndServe("localhost:9000", nil))
-
-}
-
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello world!!!")
-}
-
-func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	customer := []Customer{
-		{Name: "Forest", City: "ChengDu", ZipCode: "610000"},
-		{Name: "Aspish", City: "New Delhi", ZipCode: "11075"},
-		{Name: "Rob", City: "New Delhi", ZipCode: "11075"},
-	}
-
-	contentType := r.Header.Get("Content-Type")
-
-	if contentType == "application/xml" {
-		w.Header().Add("Content-Type", "application/xml")
-		xml.NewEncoder(w).Encode(customer)
-	} else {
-		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(customer)
-	}
-
+	app.Start()
 }
