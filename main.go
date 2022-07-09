82e274131a4eb528f21b7a3faf113d25d8cdd12e
Forest <767425412lin@gmail.com>
2022-07-09 21:13:10 +0800

    feat: marshaling data structures to json representation, and set response header

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	ZipCode string `json:"zip_code"`
+}
+
 func main() {
 	// define routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	// starting server
 	log.Fatal(http.ListenAndServe("localhost:9000", nil))
@@ -17,4 +25,16 @@ func main() {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "hello world!!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customer := []Customer{
+		{Name: "Forest", City: "ChengDu", ZipCode: "610000"},
+		{Name: "Aspish", City: "New Delhi", ZipCode: "11075"},
+		{Name: "Rob", City: "New Delhi", ZipCode: "11075"},
+	}
+
+	w.Header().Add("Content-Type", "Application/json")
+
+	json.NewEncoder(w).Encode(customer)
 }
