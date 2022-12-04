f017971362215cea17fdf87e8f2def3d0833adf6
Forest <767425412lin@gmail.com>
2022-12-04 14:15:44 +0800

    feat: go-web项目架构初始化

---
+++ routes/routes.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"go-web/logger"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Setup() *gin.Engine {
+	r := gin.New()
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, map[string]interface{}{
+			"code":    http.StatusOK,
+			"message": "ok",
+			"data":    []string{},
+		})
+	})
+
+	return r
+}
---
+++ settings/settings.go
@@ -0,0 +1,25 @@
+package settings
+
+import (
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+func Init() (err error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		fmt.Printf("Fatal error config file: %d", err)
+		return
+	}
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed:", e.Name)
+	})
+	viper.WatchConfig()
+	return nil
+}
